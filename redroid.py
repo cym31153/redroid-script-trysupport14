bb5112d10e4b0161ae3ad3d99e1939f7ed1ae2ab
Corn Pop <63576755+cym31153@users.noreply.github.com>
2024-10-12 05:12:05 -0700

    Update redroid.py

--- redroid.py
+++ redroid.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import argparse
 from stuffs.gapps import Gapps
 from stuffs.magisk import Magisk
@@ -7,7 +6,6 @@ from stuffs.ndk import Ndk
 from stuffs.widevine import Widevine
 import tools.helper as helper
 import subprocess
-
 
 def main():
     dockerfile = ""
@@ -18,7 +16,7 @@ def main():
                         dest='android',
                         help='Specify the Android version to build',
                         default='11.0.0',
-                        choices=['13.0.0', '12.0.0', '12.0.0_64only', '11.0.0', '10.0.0', '9.0.0', '8.1.0'])
+                        choices=['14.0.0', '13.0.0', '12.0.0', '12.0.0_64only', '11.0.0', '10.0.0', '9.0.0', '8.1.0'])
     parser.add_argument('-g', '--install-gapps',
                         dest='gapps',
                         help='Install OpenGapps to ReDroid',
@@ -38,18 +36,20 @@ def main():
                         default='docker',
                         help='Specify container type', 
                         choices=['docker', 'podman'])
-
     args = parser.parse_args()
+    
     dockerfile = dockerfile + \
         "FROM redroid/redroid:{}-latest\n".format(
             args.android)
     tags.append(args.android)
+    
     if args.gapps:
         Gapps().install()
         dockerfile = dockerfile + "COPY gapps /\n"
         tags.append("gapps")
+    
     if args.ndk:
-        if args.android in ["11.0.0", "12.0.0", "12.0.0_64only"]:
+        if args.android in ["11.0.0", "12.0.0", "12.0.0_64only", "13.0.0", "14.0.0"]:
             arch = helper.host()[0]
             if arch == "x86" or arch == "x86_64":
                 Ndk().install()
@@ -57,23 +57,26 @@ def main():
                 tags.append("ndk")
         else:
             helper.print_color(
-                "WARNING: Libndk seems to work only on redroid:11.0.0 or redroid:12.0.0", helper.bcolors.YELLOW)
+                "WARNING: Libndk seems to work only on redroid:11.0.0 and above", helper.bcolors.YELLOW)
+    
     if args.magisk:
         Magisk().install()
         dockerfile = dockerfile+"COPY magisk /\n"
         tags.append("magisk")
+    
     if args.widevine:
         Widevine(args.android).install()
         dockerfile = dockerfile+"COPY widevine /\n"
         tags.append("widevine")
+    
     print("\nDockerfile\n"+dockerfile)
     with open("./Dockerfile", "w") as f:
         f.write(dockerfile)
+    
     new_image_name = "redroid/redroid:"+"_".join(tags)
     subprocess.run([args.container, "build", "-t", new_image_name, "."])
     helper.print_color("Successfully built {}".format(
         new_image_name), helper.bcolors.GREEN)
-
 
 if __name__ == "__main__":
     main()
